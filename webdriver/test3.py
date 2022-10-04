a1bd587bc984fc92bcf16843cc4b625071d7b6a8
LisaWise <lisawise.mail@gmail.com>
2022-10-04 14:27:36 -0800

    webdriverhomework

---
+++ webdriver/test3.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+
+options = Options()
+options.add_argument("--no-sandbox")
+options.add_argument("headless")
+
+# START DRIVER
+# driver = webdriver.Chrome('D:\\testfolder\chromedriver.exe', options=options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.get("https://qauto2.forstudy.space/")
+
+user = "guest"
+password = "welcome2qauto"
+driver.get("https://"+user+":"+password+"@"+"qauto2.forstudy.space/")
+
+time.sleep(2)
+assert "support@ithillel.ua" in driver.page_source
+driver.close()
