34b4d43f0e602c2de652313fb187ef5d6b8f8230
LisaWise <lisawise.mail@gmail.com>
2022-10-08 13:02:35 -0800

    rehomework

---
+++ regex/test1.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+import os
+from selenium.webdriver.support import expected_conditions as EC
+
+options = Options()
+options.add_argument("--no-sandbox")
+options.add_argument("headless")
+
+# START DRIVER
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.get("https://qauto2.forstudy.space/")
+
+user = "guest"
+password = "welcome2qauto"
+driver.get("https://"+user+":"+password+"@"+"qauto2.forstudy.space/")
+
+WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a[class*='active']"))).click()
+
+time.sleep(2)
+assert "Do more!" in driver.page_source
+driver.close()
